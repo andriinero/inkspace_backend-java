51dd7d9a93c3e96f6699836ede4879801f425a51
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-11-25 21:45:55 +0100

    fix: default user role

--- src/main/java/com/andrii_nero/inkspace/user/model/User.java
+++ src/main/java/com/andrii_nero/inkspace/user/model/User.java
@@ -2,6 +2,7 @@ package com.andrii_nero.inkspace.user.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     @Id
@@ -25,10 +27,11 @@ public class User {
 
     private String email;
 
+    @Column(columnDefinition = "varchar(255) default 'USER'")
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    @Column(name = "sign_up_date", columnDefinition = "varchar(255) default 'USER'")
+    @Column(name = "sign_up_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date signUpDate;
 
