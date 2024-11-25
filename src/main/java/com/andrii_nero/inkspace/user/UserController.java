6069a95f52f0c24ce1ad26166531700e33a3bc96
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-11-25 21:45:38 +0100

    feat: user endpoints

--- src/main/java/com/andrii_nero/inkspace/user/UserController.java
+++ src/main/java/com/andrii_nero/inkspace/user/UserController.java
@@ -1,13 +1,12 @@
 package com.andrii_nero.inkspace.user;
 
 import com.andrii_nero.inkspace.user.dto.PostUserRequest;
-import com.andrii_nero.inkspace.user.model.User;
+import com.andrii_nero.inkspace.user.dto.UserCreationDTO;
+import com.andrii_nero.inkspace.user.dto.UserDTO;
+import com.andrii_nero.inkspace.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,16 +18,31 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> userList = userService.getAllUsers();
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> userList = userService.getAllUsers();
 
         return ResponseEntity.ok(userList);
     }
 
-    @PostMapping
-    public ResponseEntity<String> postUser(PostUserRequest request) {
-        userService.saveUser(request);
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id) {
+        UserDTO userDTO = userService.getUserById(id);
 
-        return ResponseEntity.ok("Successfully posted user");
+        return ResponseEntity.ok(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postUser(PostUserRequest request) {
+        UserCreationDTO userCreationDTO = UserMapper.postUserRequestToUserCreationDTO(request);
+        userService.saveUser(userCreationDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable String id) {
+        userService.deleteUserById(id);
+
+        return ResponseEntity.ok().build();
     }
 }
