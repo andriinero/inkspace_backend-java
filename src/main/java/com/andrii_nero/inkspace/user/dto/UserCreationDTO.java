4e01ca604819313def767132d3acfb9738c93ce4
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-11-25 22:29:57 +0100

    fix: formatting

--- src/main/java/com/andrii_nero/inkspace/user/dto/UserCreationDTO.java
+++ src/main/java/com/andrii_nero/inkspace/user/dto/UserCreationDTO.java
@@ -1,7 +1,8 @@
 package com.andrii_nero.inkspace.user.dto;
 
-public record UserCreationDTO(String username,
-                              String password,
-                              String email,
-                              String profileImage) {
+public record UserCreationDTO(
+        String username,
+        String password,
+        String email,
+        String profileImage) {
 }
--- src/main/java/com/andrii_nero/inkspace/user/exception/UserNotFoundException.java
+++ src/main/java/com/andrii_nero/inkspace/user/exception/UserNotFoundException.java
@@ -1,9 +1,9 @@
 package com.andrii_nero.inkspace.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    private static final String USER_NOT_FOUND_MESSAGE = "User with (id: %s) not found";
+    private static final String USER_NOT_FOUND_TEMPLATE = "User with (id: %s) not found";
 
     public UserNotFoundException(String id) {
-        super(String.format(USER_NOT_FOUND_MESSAGE, id));
+        super(String.format(USER_NOT_FOUND_TEMPLATE, id));
     }
 }
